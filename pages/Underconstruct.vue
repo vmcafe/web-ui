eaed3667242b6f96eac10cec4b3958c3c25b6ab3
FitriaSulistiya <fitriasulistiya@gmail.com>
2021-03-10 19:54:12 +0700

    underconstruct lanjutan

--- pages/Underconstruct.vue
+++ pages/Underconstruct.vue
@@ -1,13 +1,95 @@
 <template>
-  <div class="underconstruction">Hallo VMC</div>
+  <div class="underconstruct">
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="4000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <!-- Text slides with image -->
+      <b-carousel-slide
+        caption="First slide"
+        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+        img-src="https://picsum.photos/1024/480/?image=52"
+      ></b-carousel-slide>
+
+      <!-- Slides with custom text -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+        <h1>Hello world!</h1>
+      </b-carousel-slide>
+
+      <!-- Slides with image only -->
+      <b-carousel-slide
+        img-src="https://picsum.photos/1024/480/?image=58"
+      ></b-carousel-slide>
+
+      <!-- Slides with img slot -->
+      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+      <b-carousel-slide>
+        <template #img>
+          <img
+            class="d-block img-fluid w-100"
+            width="1024"
+            height="480"
+            src="https://picsum.photos/1024/480/?image=55"
+            alt="image slot"
+          />
+        </template>
+      </b-carousel-slide>
+
+      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
+      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          eros felis, tincidunt a tincidunt eget, convallis vel est. Ut
+          pellentesque ut lacus vel interdum.
+        </p>
+      </b-carousel-slide>
+    </b-carousel>
+
+    <div class="judul">WE'RE LAUNCHING SOON</div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      slide: 0,
+      sliding: null,
+    }
+  },
+  methods: {
+    onSlideStart(slide) {
+      this.sliding = true
+    },
+    onSlideEnd(slide) {
+      this.sliding = false
+    },
+  },
+}
 </script>
 
 <style lang="stylus" scoped>
-.underconstruction
-  background-image url('~assets/bg.jpeg')
-  min-height 100vh
+@import '../styles/imports'
+.underconstruct
+  position relative
+
+.judul
+  position absolute
+  top 0
+  left 0
+  right 0
+  bottom 0
+  height 100vh
+  display flex
+  align-items center
+  justify-content center
 </style>
