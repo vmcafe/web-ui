7545cc9a13fbb30db2ad5dd0b2cdaed1889281d0
Safria Isnibaiti <safriaisnibaiti637@gmail.com>
2021-03-12 19:31:42 +0700

    Update underconstruct

--- pages/Underconstruct.vue
+++ pages/Underconstruct.vue
@@ -13,16 +13,26 @@
     >
       <b-carousel-slide v-for="item in [1, 2, 3, 4]" :key="item">
         <template #img>
-          <h1 class="judul">WE'RE LAUNCHING SOON</h1>
-          <p class="desc1">
+          <div class="logo-underconstruct">
+            <img
+              class="logo d-block mx-auto"
+              width="170"
+              height="170"
+              src="~/assets/Logo2.png"
+              alt=""
+            />
+          </div>
+
+          <h1 class="judul mx-auto">WE'RE LAUNCHING SOON</h1>
+          <p class="desc1 mx-auto">
             Please kindly wait for us. We're doing a super great job here and we
             need time to construct it :(
           </p>
-          <p class="desc2">
+          <p class="desc2 mx-auto">
             Don't leave us pwis, we will provide you something exciting nyaa :3
           </p>
-          <p class="desc3">Stay connected</p>
-          <p class="desc4">©2020. All right reserved</p>
+          <p class="desc3 mx-auto">Stay connected</p>
+          <p class="desc4 mx-auto">©2020. All right reserved</p>
           <img
             class="d-block img-fluid w-100"
             width="1024"
@@ -30,6 +40,17 @@
             :src="require(`~/assets/undercontruct${item}.png`)"
             alt="image slot"
           />
+          <div class="footer_list_media">
+            <a href="/" target="blank" class="footer_listmedia_item">
+              <img src="~/assets/icons/ic_fb.svg" alt="" />
+            </a>
+            <a href="/" target="blank" class="footer_listmedia_item">
+              <img src="~/assets/icons/ic_ig.svg" alt="" />
+            </a>
+            <a href="/" target="blank" class="footer_listmedia_item">
+              <img src="~/assets/icons/linkedin.svg" alt="" />
+            </a>
+          </div>
         </template>
       </b-carousel-slide>
     </b-carousel>
@@ -60,9 +81,19 @@ export default {
 .underconstruct
   position relative
 
+.logo
+  top 20px
+  left 0
+  right 0
+  bottom 0
+  display flex
+  position absolute
+  align-items center
+  justify-content center
+
 .judul
   position absolute
-  top 0
+  top -40px
   left 0
   right 0
   bottom 0
@@ -78,57 +109,57 @@ export default {
 
 .desc1
   position absolute
-  top 80px
-  left 100px
-  right 100px
+  top 20px
+  left 0
+  right 0
   bottom 0
   height 100vh
   display flex
   align-items center
   justify-content center
-  font-size 50px
-  font 18px/17px segoe-ui
+  font-size 18px
+  font normal normal segoe-ui
   letter-spacing 1.8px
   color #2D4957
   opacity 1
 
 .desc2
   position absolute
-  top 110px
-  left 100px
-  right 100px
+  top 45px
+  left 0
+  right 0
   bottom 0
   height 100vh
   display flex
   align-items center
   justify-content center
-  font-size 50px
-  font 18px/17px segoe-ui
+  font-size 18px
+  font normal normal segoe-ui
   letter-spacing 1.8px
   color #2D4957
   opacity 1
 
 .desc3
   position absolute
-  top 260px
-  left 100px
-  right 100px
+  top 160px
+  left 0
+  right 0
   bottom 0
   height 100vh
   display flex
   align-items center
   justify-content center
-  font-size 50px
-  font 18px/17px segoe-ui
+  font-size 18px
+  font normal normal segoe-ui
   letter-spacing 1.8px
   color #2D4957
   opacity 1
 
 .desc4
   position absolute
-  top 450px
-  left 100px
-  right 100px
+  top 270px
+  left 0
+  right 0
   bottom 0
   height 100vh
   display flex
@@ -139,4 +170,15 @@ export default {
   letter-spacing 1.2px
   color #000000
   opacity 1
+
+.footer_listmedia_item
+  position absolute
+  top 190px
+  left 0
+  right 0
+  bottom 0
+  height 100vh
+  display flex
+  align-items center
+  justify-content center
 </style>
